c91497f8a27340854d1b2f7a9e67efd706e8d496
VaishnaviSamudrala12 <vaishnavisamudrala276@gmail.com>
2026-07-28 15:06:54 +0530

    Tokens

---
+++ Day1.py
@@ -0,0 +1,20 @@
+print("Hello Codegnan")
+'''
+print("vaishnavi")
+'''
+name="vaishnavi"
+print(name)
+email_id="vaishnavisamudrala62@gmail.com"
+print(email_id)
+vidya="jashnavi"
+print(vidya)
+#tokens example together
+length=3
+breadth=6
+area=length*breadth
+print(area)
+#multiassignment of variables
+name,location,age=("vaishnavi","hyd",21)
+print(name)
+print(age)
+print(location)
